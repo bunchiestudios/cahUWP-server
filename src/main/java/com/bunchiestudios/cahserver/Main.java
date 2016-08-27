ef2453c67ed7b7e35f804b3ce0f9cda427f44ef8
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-08-27 13:02:06 -0500

    Added logging

--- src/main/java/com/bunchiestudios/cahserver/Main.java
+++ src/main/java/com/bunchiestudios/cahserver/Main.java
@@ -1,15 +1,20 @@
 package com.bunchiestudios.cahserver;
 
+import org.apache.log4j.Logger;
+
 /**
  * Created by rdelfin on 8/27/16.
  */
 public class Main {
+    private static Logger log = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
         try {
             Server server = new Server();
             server.start();
-        } catch(Exception e) {
-
+        } catch (Exception e) {
+            log.error("There was an exception!");
+            log.error(e.getMessage());
         }
     }
 }
