99df4b93eaa884dda2b62ad87ce11c30d69e0309
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-08-27 21:28:21 -0500

    Removed unused template

--- src/main/java/com/bunchiestudios/cahserver/Request.java
+++ src/main/java/com/bunchiestudios/cahserver/Request.java
@@ -5,7 +5,7 @@ import org.json.JSONObject;
 /**
  * Created by rdelfin on 8/27/16.
  */
-public interface Request<M> {
+public interface Request {
     public JSONObject perform(JSONObject message);
     public String getName();
 }
