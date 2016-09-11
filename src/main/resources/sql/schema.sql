dcf9af51de407a7c84007a71bed63477be70b433
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-09-11 17:31:13 -0500

    New schema

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,7 +1,9 @@
 CREATE SEQUENCE game_id_seq;
 CREATE TABLE game (
   id bigint CONSTRAINT mainkey_game PRIMARY KEY DEFAULT nextval('game_id_seq'),
-  name varchar(255) UNIQUE
+  name varchar(255) UNIQUE NOT NULL,
+  black_deck bigint REFERENCES game_cards (card_id),
+  white_deck bigint REFERENCES game_cards (card_id)
 );
 
 CREATE SEQUENCE player_id_seq;
@@ -23,7 +25,8 @@ CREATE TABLE cards (
 CREATE TABLE game_cards (
   card_id bigint NOT NULL REFERENCES cards (id),
   game_id bigint NOT NULL REFERENCES game (id),
-  daisy_chain_card bigint NOT NULL,
-  daisy_chain_game bigint NOT NULL,
-  status smallint NOT NULL
+  player_id bigint REFERENCES player (id),                      -- Playing inferred by whether this is null
+  daisy_chain_winner bigint REFERENCES game_cards (card_id),    -- Used for both winner if this is a black card, or next card when playing n cards
+  daisy_chain_game bigint REFERENCES game_cards (card_id),      -- Used to define the deck
+  status smallint NOT NULL                                      -- First bit: played
 );
