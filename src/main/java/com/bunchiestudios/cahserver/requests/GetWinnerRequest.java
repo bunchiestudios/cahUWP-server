a2fd15724f4f931ca8e09b1dbc53059ceae7d25e
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-10-07 00:38:06 -0500

    Added schema paths to all requests

--- src/main/java/com/bunchiestudios/cahserver/requests/GetWinnerRequest.java
+++ src/main/java/com/bunchiestudios/cahserver/requests/GetWinnerRequest.java
@@ -3,14 +3,15 @@ package com.bunchiestudios.cahserver.requests;
 import com.bunchiestudios.cahserver.RequestIdentifier;
 import com.bunchiestudios.cahserver.ServerRequest;
 import com.bunchiestudios.cahserver.database.DataManager;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 import org.json.JSONObject;
 
 /**
  * Created by rdelfin on 8/27/16.
  */
 public class GetWinnerRequest extends ServerRequest {
-    public GetWinnerRequest(DataManager mgr) {
-        super(mgr);
+    public GetWinnerRequest(DataManager mgr) throws ProcessingException {
+        super(mgr, "resource:json/schema/request/GetWinner.json", "resource:json/schema/response/GetWinner.json");
     }
 
     @Override
