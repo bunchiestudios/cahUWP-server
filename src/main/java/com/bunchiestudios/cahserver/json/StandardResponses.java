6f2078ac5cb126abd16f8bbe83d662f9618511d9
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-10-07 10:02:51 -0500

    Created StandardResponses class

---
+++ src/main/java/com/bunchiestudios/cahserver/json/StandardResponses.java
@@ -0,0 +1,12 @@
+package com.bunchiestudios.cahserver.json;
+
+import org.json.JSONObject;
+
+/**
+ * Created by rdelfin on 10/7/16.
+ */
+public class StandardResponses {
+    public static JSONObject userUnauthenticatedError() {
+
+    }
+}
