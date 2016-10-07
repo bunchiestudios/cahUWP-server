f867f5f466f8e1d6e34ec39a6da6c03e7cbfca93
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-10-07 10:12:35 -0500

    Added first few standard errors

--- src/main/java/com/bunchiestudios/cahserver/json/StandardResponses.java
+++ src/main/java/com/bunchiestudios/cahserver/json/StandardResponses.java
@@ -6,7 +6,36 @@ import org.json.JSONObject;
  * Created by rdelfin on 10/7/16.
  */
 public class StandardResponses {
-    public static JSONObject userUnauthenticatedError() {
+    public static JSONObject messageFormatInvalidError() {
+        JSONObject result = new JSONObject();
+        result.put("error", "Message format invalid. Check API documentation.");
+        result.put("error_code", 100);
 
+        return result;
+    }
+
+    public static JSONObject userUnauthenticatedError() {
+        JSONObject result = new JSONObject();
+        result.put("error", "User token is invalid or user does not exist.");
+        result.put("error_code", 101);
+
+        return result;
+    }
+
+    public static JSONObject createGameError() {
+        JSONObject result = new JSONObject();
+        result.put("error", "There was an error creating the game.");
+        result.put("error_code", 102);
+
+        return result;
+
+    }
+
+    public static JSONObject unknownError(String action) {
+        JSONObject result = new JSONObject();
+        result.put("error", "There was an unknown error when " + action + ".");
+        result.put("error_code", 103);
+
+        return result;
     }
 }
